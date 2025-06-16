f23110d3710119c65d12098cac7faa361ce16c77
alejocode96 <alejocode96@gmail.com>
2025-06-16 17:41:49 -0500

    init proyect

---
+++ src/Pages/App/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='bg-amber-200'>INIT PROYECT</p>
+      
+    </>
+  )
+}
+
+export default App
