abb40d19930c77f125359829991e292539506b8d
alejocode96 <alejocode96@gmail.com>
2025-06-16 23:40:04 -0500

    cambios colores header

--- src/Components/Home/header.jsx
+++ src/Components/Home/header.jsx
@@ -29,13 +29,12 @@ function Header() {
                     {/* Botón de inicio de sesión */}
                     <button className="hidden lg:block group relative rounded-full p-px text-sm/6 text-zinc-400 duration-300 dark:hover:text-zinc-100 hover:text-zinc-500 hover:shadow-glow">
                         <span className="absolute inset-0 overflow-hidden rounded-full">
-                            <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+                            <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(108,194,74,0.6)_0%,rgba(108,194,74,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
                         </span>
                         <div className="relative z-10 rounded-full bg-white/50 dark:bg-black bg-opacity-20 backdrop-blur-xl px-4 py-1.5 ring-1 ring-white/10">
                             Iniciar sesión
                         </div>
-                        <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-cyan-400/0 via-cyan-400/90 to-cyan-400/0 transition-opacity duration-500 group-hover:opacity-40" />
-                    </button>
+                        <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-[#6CC24A]/0 via-[#6CC24A]/90 to-[#6CC24A]/0 transition-opacity duration-500 group-hover:opacity-40" />                    </button>
 
                     {/* Botón de modo oscuro */}
                     <button onClick={toggleDarkMode}
@@ -73,13 +72,13 @@ function Header() {
                         {/* Botón de inicio de sesión */}
                         <button className="block lg:hidden group relative rounded-full p-px text-sm/6 text-zinc-500 duration-300 hover:text-zinc-800 dark:hover:text-zinc-100 hover:shadow-glow">
                             <span className="absolute inset-0 overflow-hidden rounded-full">
-                                <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+                                <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(108,194,74,0.6)_0%,rgba(108,194,74,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
                             </span>
                             <div className="relative z-10 rounded-full bg-zinc-900/5 dark:bg-black bg-opacity-20 backdrop-blur-xl px-4 py-1.5 ring-1 ring-white/10">
                                 Iniciar sesión
                             </div>
-                            <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-cyan-400/0 via-cyan-400/90 to-cyan-400/0 transition-opacity duration-500 group-hover:opacity-40" />
-                        </button>
+                            <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-[#6CC24A]/0 via-[#6CC24A]/90 to-[#6CC24A]/0 transition-opacity duration-500 group-hover:opacity-40" />                        </button>
+
                     </div>
                     <div className='group pt-16 z-60'>
                         <a className='flex flex-col items-center gap-1' href='#Section_WhoIsLogistico'>
